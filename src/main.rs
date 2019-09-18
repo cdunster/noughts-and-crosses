27265ad1fecddc9d4976024975d404a202e8abca
Callum Dunster <cdunster@users.noreply.github.com>
2019-09-19 00:04:58 +0200

    Add function for user to select a side.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+fn select_side() -> char {
+    loop {
+        println!("Would you like to be O's or X's?");
+        let mut side = String::new();
+        io::stdin().read_line(&mut side).unwrap();
+        let side = side.trim().to_ascii_uppercase();
+        match side.as_str() {
+            "X" => return 'X',
+            "O" => return 'O',
+            _ => println!("{} is and invalid choice, please type O or X.", side),
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Noughts and Crosses!");
+    let side = select_side();
 }
