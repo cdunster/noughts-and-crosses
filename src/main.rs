7a946f7c495a4f127be88458f01c87c88471b9b7
Callum Dunster <cdunster@users.noreply.github.com>
2019-09-19 00:06:20 +0200

    Add a grid and draw it.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+static mut GRID: [[char; 3]; 3] = [[' '; 3]; 3];
+
 fn select_side() -> char {
     loop {
         println!("Would you like to be O's or X's?");
@@ -13,7 +15,18 @@ fn select_side() -> char {
         }
     }
 }
+
+fn draw_grid() {
+    unsafe {
+        println!("{}|{}|{}", GRID[0][0], GRID[0][1], GRID[0][2]);
+        println!("_____");
+        println!("{}|{}|{}", GRID[1][0], GRID[1][1], GRID[1][2]);
+        println!("_____");
+        println!("{}|{}|{}", GRID[2][0], GRID[2][1], GRID[2][2]);
+    }
+}
 fn main() {
     println!("Welcome to Noughts and Crosses!");
     let side = select_side();
+    draw_grid();
 }
